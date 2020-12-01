15fc6516a09f699f87639ba1401656317756664c
Mariana Garcia <marianagarcia901974@gmail.com>
2020-11-30 22:01:28 -0300

    Hero e start sidebar

---
+++ src/components/Sidebar/SidebarElements.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { FaTImes} from 'react-icons/fa';
+
+
+export const SidebarContainer = styled.aside`
+    position: fixed;
+    z-index: 999;
+    width: 350px;
+    height: 100%100vh;
+    background: #ffc500;
+    display: grid;
+    align-items: center;
+    top: 0;
+    transition: 0.3s ease-ease-in-out;
+    right: ;
+`;
+
+export const Icon = styled.div``;
+export const CloseIcon = styled.div``;
+export const SidebarMenu = styled.div``;
+export const SidebarLink = styled.div``;
+export const SideBtnWrap = styled.div``;
+export const SidebarRoute = styled.div``;
